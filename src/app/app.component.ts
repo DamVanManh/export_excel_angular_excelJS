836f2b8031a5e885c99171f9ca4db4ad5c5dd36d
Dam Van Manh <hnammad7@gmail.com>
2021-12-17 17:33:42 +0700

    Make the functions be reusability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,132 +8,359 @@ import { ExcelService } from "./excel.service";
 })
 export class AppComponent {
   constructor(private excelService: ExcelService) {}
-  data = [
-    [
-      "",
-      36,
-      "Conductor",
-      "Casing",
-      30,
-      534.8,
-      984.3,
-      543.8,
-      534.8,
-      984.3,
-      543.8,
-      8.35,
-      "",
-      "",
-      "Failed to load",
-    ],
-    [
-      "",
-      36,
-      "Conductor",
-      "Casing",
-      30,
-      534.8,
-      984.3,
-      543.8,
-      534.8,
-      984.3,
-      543.8,
-      8.35,
-      "",
-      "",
-      "Failed to load",
-    ],
-    [
-      "",
-      36,
-      "Conductor",
-      "Casing",
-      30,
-      534.8,
-      984.3,
-      543.8,
-      534.8,
-      984.3,
-      543.8,
-      8.35,
-      "",
-      "",
-      "Failed to load",
-    ],
-    [
-      "",
-      36,
-      "Conductor",
-      "Casing",
-      30,
-      534.8,
-      984.3,
-      543.8,
-      534.8,
-      984.3,
-      543.8,
-      8.35,
-      "",
-      "",
-      "Failed to load",
-    ],
-    [
-      "",
-      36,
-      "Conductor",
-      "Casing",
-      30,
-      534.8,
-      984.3,
-      543.8,
-      534.8,
-      984.3,
-      543.8,
-      8.35,
-      "",
-      "",
-      "Failed to load",
-    ],
-  ];
+
   generateExcel() {
-    // this.excelService.generateExcel();
-    this.excelService.exportToExcelHoldSections({
-      myData: this.data,
-      fileName: "Hole Sections Summary",
-      sheetName: "sheet1",
-      sheetTitle: "Hole Sections Summary",
-      tableTitle: "Hole Sections",
-      myHeader: [
+    const data = [
+      [
+        "",
+        36,
+        "Conductor",
+        "Casing",
+        30,
+        534.8,
+        984.3,
+        543.8,
+        534.8,
+        984.3,
+        543.8,
+        8.35,
+        "",
+        "",
+        "Failed to load",
+      ],
+      [
+        "",
+        36,
+        "Conductor",
+        "Casing",
+        30,
+        534.8,
+        984.3,
+        543.8,
+        534.8,
+        984.3,
+        543.8,
+        8.35,
+        "",
+        "",
+        "Failed to load",
+      ],
+      [
+        "",
+        36,
+        "Conductor",
+        "Casing",
+        30,
+        534.8,
+        984.3,
+        543.8,
+        534.8,
+        984.3,
+        543.8,
+        8.35,
+        "",
+        "",
+        "Failed to load",
+      ],
+      [
+        "",
+        36,
+        "Conductor",
+        "Casing",
+        30,
+        534.8,
+        984.3,
+        543.8,
+        534.8,
+        984.3,
+        543.8,
+        8.35,
+        "",
+        "",
+        "Failed to load",
+      ],
+      [
+        "",
+        36,
+        "Conductor",
+        "Casing",
+        30,
+        534.8,
+        984.3,
+        543.8,
+        534.8,
+        984.3,
+        543.8,
+        8.35,
+        "",
+        "",
+        "Failed to load",
+      ],
+    ];
+    const data2 = [
+      [
+        "Conductor casing",
+        534.8,
+        984.3,
+        30,
+        310.0,
+        "X56M",
+        "VIPER-3ST (M70)",
+        "N/A",
+        "N/A",
+        "N/A",
+        "N/A",
+      ],
+      [
+        "Conductor casing",
+        534.8,
+        984.3,
+        30,
+        310.0,
+        "X56M",
+        "VIPER-3ST (M70)",
+        "N/A",
+        "N/A",
+        "N/A",
+        "N/A",
+      ],
+      [
+        "Conductor casing",
+        534.8,
+        984.3,
+        30,
+        310.0,
+        "X56M",
+        "VIPER-3ST (M70)",
+        "N/A",
+        "N/A",
+        "N/A",
+        "N/A",
+      ],
+      [
+        "Conductor casing",
+        534.8,
+        984.3,
+        30,
+        310.0,
+        "X56M",
+        "VIPER-3ST (M70)",
+        "N/A",
+        "N/A",
+        "N/A",
+        "N/A",
+      ],
+    ];
+    const STYLE_SHEET_TITLE = {
+      border: false,
+      height: 40,
+      font: { size: 30, bold: true, color: { argb: "333333" } },
+      alignment: { horizontal: "left", vertical: "middle" },
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "ffffff" },
+      },
+    };
+    const STYLE_TABLE_TITLE = {
+      border: true,
+      height: 40,
+      font: { size: 20, bold: false, color: { argb: "333333" } },
+      alignment: { horizontal: "center", vertical: "middle" },
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "f4f4f4" },
+      },
+    };
+    const STYLE_DATA_BLACK = {
+      border: true,
+      height: 45,
+      font: { size: 15, bold: false, color: { argb: "333333" } },
+      alignment: { horizontal: "center", vertical: "middle" },
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "d1d1d1" },
+      },
+    };
+    const wb = this.excelService.generateWorkbook();
+    const sheet1 = this.excelService.addWorksheet(wb, "Hole Sections");
+    this.excelService.styleWidthColumns(sheet1, [
+      { width: 12 },
+      { width: 10 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 15 },
+      { width: 20 },
+    ]);
+    this.excelService.addRowTitle(
+      sheet1,
+      "Hole Sections Summary",
+      1,
+      15,
+      STYLE_SHEET_TITLE
+    );
+    this.excelService.addRowTitle(
+      sheet1,
+      "Hole Sections",
+      1,
+      15,
+      STYLE_TABLE_TITLE
+    );
+    this.excelService.addHeader(
+      sheet1,
+      this.excelService.STYLE_HEADER,
+      [
         "",
         "Hole Size(in)",
         "Name",
         "String Type",
         "Casing OD(in)",
         "Measured Depth (ft)",
+        "",
+        "",
         "TVD (ft)",
+        "",
+        "",
         "Mud Density (ppg)",
         "Fluid Type",
         "Lithology at Shoe",
         "Kick Tolerance (bbl)",
       ],
-      bottomHeader: ["Top", "Shoe", "TOC"],
-      widths: [
-        { width: 12 },
-        { width: 10 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 15 },
-        { width: 20 },
+      ["", "", "", "", "", "Top", "Shoe", "TOC", "Top", "Shoe", "TOC"]
+    );
+    this.excelService.addData(sheet1, data, STYLE_DATA_BLACK);
+
+    const sheet2 = this.excelService.addWorksheet(wb, "Casing Design");
+    this.excelService.styleWidthColumns(sheet2, [
+      { width: 18 },
+      { width: 13 },
+      { width: 13 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+      { width: 18 },
+    ]);
+    this.excelService.addRowTitle(
+      sheet2,
+      "Casing Design",
+      1,
+      11,
+      STYLE_TABLE_TITLE
+    );
+    this.excelService.addHeader(
+      sheet2,
+      this.excelService.STYLE_HEADER,
+      [
+        "Name/Type",
+        "Top MD (ft)",
+        "Base MD (ft)",
+        "OD (in)",
+        "Weight (ppf)",
+        "Grade",
+        "Connection",
+        "Absolute Safety Factors",
+        "",
+        "",
+        "",
       ],
-    });
+      ["", "", "", "", "", "", "", "Burst", "Collapse", "Axial", "Triaxial"]
+    );
+    this.excelService.addData(sheet2, data2, STYLE_DATA_BLACK);
+
+    this.excelService.saveAsExcelFile(wb, "Hole Sections Summary");
+  }
+  generateExcel2() {
+    const data1 = [
+      ["Jet A1", 2.7, "N/A"],
+      ["Jet A1", 2.7, "N/A"],
+      ["Jet A1", 2.7, "N/A"],
+      ["Jet A1", 2.7, "N/A"],
+      ["Jet A1", 7, "N/A"],
+    ];
+    const data2 = [["Temperature Gain Effect", 5.0, "N/A"]];
+    const data3 = [
+      ["Dealer Margin", 5.0, 0.71],
+      ["Alpha", 5.0, 0.71],
+    ];
+    const STYLE_HEADER2 = {
+      border: {
+        top: { style: "thin", color: { argb: "000000" } },
+        left: { style: "thin", color: { argb: "000000" } },
+        bottom: { style: "thin", color: { argb: "000000" } },
+        right: { style: "thin", color: { argb: "000000" } },
+      },
+      height: 35,
+      font: { size: 15, bold: true, color: { argb: "ffffff" } },
+      alignment: { horizontal: "center", vertical: "middle" },
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "579497" },
+      },
+    };
+    const STYLE_DATA2 = {
+      border: {
+        top: { style: "thin", color: { argb: "000000" } },
+        left: { style: "thin", color: { argb: "000000" } },
+        bottom: { style: "thin", color: { argb: "000000" } },
+        right: { style: "thin", color: { argb: "000000" } },
+      },
+      height: 15,
+      font: { size: 15, bold: true, color: { argb: "000000" } },
+      alignment: { horizontal: "center", vertical: "middle" },
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "ffffff" },
+      },
+    };
+    const wb = this.excelService.generateWorkbook();
+    const sheet1 = this.excelService.addWorksheet(wb, "Conversion factor");
+    this.excelService.styleWidthColumns(sheet1, [
+      { width: 50 },
+      { width: 30 },
+      { width: 30 },
+    ]);
+    this.excelService.addRowTitle(
+      sheet1,
+      "CONVERSION FACTOR",
+      1,
+      3,
+      this.excelService.STYLE_HEADER
+    );
+    this.excelService.addHeader(sheet1, STYLE_HEADER2, [
+      "CONVERSION FACTOR",
+      "FACTOR",
+      "USD/BBL",
+    ]);
+    this.excelService.addData(sheet1, data1, STYLE_DATA2);
+    this.excelService.addHeader(sheet1, STYLE_HEADER2, [
+      "EFFECTS",
+      "FACTOR",
+      "USD/BBL",
+    ]);
+    this.excelService.addData(sheet1, data2, STYLE_DATA2);
+    this.excelService.addHeader(sheet1, STYLE_HEADER2, [
+      "GASOLINE U95",
+      "US CENT/LITER",
+      "USD/BBL",
+    ]);
+    this.excelService.addData(sheet1, data3, STYLE_DATA2);
+    this.excelService.saveAsExcelFile(wb, "Conversion factor");
   }
 }
--- src/app/excel.service.ts
+++ src/app/excel.service.ts
@@ -6,28 +6,10 @@ import * as fs from "file-saver";
 })
 export class ExcelService {
   constructor() {}
-  STYLE_SHEET_TITLE = {
-    border: false,
-    height: 40,
-    font: { size: 30, bold: true, color: { argb: "333333" } },
-    alignment: { horizontal: "left", vertical: "middle" },
-    fill: {
-      type: "pattern",
-      pattern: "solid",
-      fgColor: { argb: "ffffff" },
-    },
-  };
-  STYLE_TABLE_TITLE = {
-    border: true,
-    height: 40,
-    font: { size: 20, bold: false, color: { argb: "333333" } },
-    alignment: { horizontal: "center", vertical: "middle" },
-    fill: {
-      type: "pattern",
-      pattern: "solid",
-      fgColor: { argb: "f4f4f4" },
-    },
-  };
+  EXCEL_TYPE =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  EXCEL_EXTENSION = ".xlsx";
+
   STYLE_HEADER = {
     border: true,
     height: 35,
@@ -50,17 +32,7 @@ export class ExcelService {
       fgColor: { argb: "ff0000" },
     },
   };
-  STYLE_DATA_BLACK = {
-    border: true,
-    height: 45,
-    font: { size: 15, bold: false, color: { argb: "333333" } },
-    alignment: { horizontal: "center", vertical: "middle" },
-    fill: {
-      type: "pattern",
-      pattern: "solid",
-      fgColor: { argb: "d1d1d1" },
-    },
-  };
+
   STYLE_DATA_WARNING = {
     border: true,
     height: 70,
@@ -72,75 +44,58 @@ export class ExcelService {
       fgColor: { argb: "ff0000" },
     },
   };
-  async exportToExcelHoldSections({
-    myData,
-    fileName,
-    sheetName,
-    sheetTitle,
-    tableTitle,
-    myHeader,
-    bottomHeader,
-    widths,
-  }) {
-    if (!myData || myData.length === 0) {
-      console.error("empty data");
-      return;
-    }
-    // console.log("exportToExcel ", myData);
-    const wb = new Workbook();
-    const ws = wb.addWorksheet(sheetName);
-    const columns = 15;
-    const data = {
-      border: true,
-      money: true,
-      height: 0,
-      font: { size: 12, bold: false, color: { argb: "000000" } },
-      alignment: null,
-      fill: null,
-    };
-    // set width từng colume
-    if (widths && widths.length > 0) {
-      ws.columns = widths;
-    }
-    // tạo ô chứa SheetTitle và merge thành một ô
-    let rowSheetTitle = this.addRow(ws, [sheetTitle], this.STYLE_SHEET_TITLE);
-    this.mergeRowCells(ws, rowSheetTitle, 1, columns);
-    // tạo ô chứa TableTitle và merge thành một ô
-    let rowTableTitle = this.addRow(ws, [tableTitle], this.STYLE_TABLE_TITLE);
-    this.mergeRowCells(ws, rowTableTitle, 1, columns);
 
-    // tạo header
-    const rowHeader = this.addRow(ws, myHeader, this.STYLE_HEADER);
-    rowHeader.splice(7, 0, "", "");
-    rowHeader.splice(10, 0, "", "");
-    this.mergeRowCells(ws, rowHeader, 6, 8);
-    this.mergeRowCells(ws, rowHeader, 9, 11);
-    // // header dưới
-    let rowHeader2 = ws.addRow([
-      "",
-      "",
-      "",
-      "",
-      "",
-      ...bottomHeader,
-      ...bottomHeader,
-    ]);
-    this.styleRowCell(rowHeader2, this.STYLE_HEADER);
-    // merge dọc header
-    rowHeader.eachCell({ includeEmpty: true }, function (cell, colNumber) {
-      const nameOfUnderCell = `${cell._column.letter}${cell._row._number + 1}`;
-      const isUnderCellHasValue = ws.getCell(nameOfUnderCell).value;
-      if (!isUnderCellHasValue) {
-        ws.mergeCells(`${cell._address}:${nameOfUnderCell}`);
+  public addData(workSheet: any, data: any[][], style: any) {
+    data.forEach((row) => {
+      this.addRow(workSheet, row, style);
+    });
+  }
+  public addHeader(
+    ws: any,
+    style: any,
+    header: string[],
+    bottomHeader?: string[]
+  ) {
+    const rowHeader = this.addRow(ws, header, style);
+    let rowBottomHeader;
+    // merge empty cell horizontal
+    for (let indexContent = 0; indexContent < header.length; indexContent++) {
+      if (header[indexContent] === "" && indexContent > 0) {
+        const cellFrom = indexContent;
+        let cellTo = 0;
+        for (let index = indexContent + 1; index < header.length + 1; index++) {
+          console.log(
+            "vào tới ",
+            indexContent,
+            index,
+            header[index],
+            header.length
+          );
+
+          if (header[index] !== "" || index === header.length) {
+            cellTo = index;
+            indexContent = index;
+            this.mergeRowCells(ws, rowHeader, cellFrom, cellTo);
+            break;
+          }
+        }
       }
-      cell;
-    });
-    myData.forEach((row) => {
-      this.addRow(ws, row, this.STYLE_DATA_BLACK);
-    });
-
-    const buf = await wb.xlsx.writeBuffer();
-    fs.saveAs(new Blob([buf]), `${fileName}.xlsx`);
+    }
+    // merge empty cell vertical
+    if (bottomHeader && bottomHeader.length > 0) {
+      rowBottomHeader = this.addRow(ws, bottomHeader, style);
+      rowHeader.eachCell({ includeEmpty: true }, function (cell, colNumber) {
+        const nameOfUnderCell = `${cell._column.letter}${
+          cell._row._number + 1
+        }`;
+        const isUnderCellHasValue = ws.getCell(nameOfUnderCell).value;
+        if (!isUnderCellHasValue) {
+          ws.mergeCells(`${cell._address}:${nameOfUnderCell}`);
+        }
+        cell;
+      });
+    }
+    return { rowHeader, rowBottomHeader };
   }
   private addRow(ws, data, style) {
     const row = ws.addRow(data);
@@ -156,10 +111,10 @@ export class ExcelService {
     };
     row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
       if (style.border) {
-        cell.border = borderStyles;
+        cell.border = { ...borderStyles, ...style.border };
       }
       if (style.alignment) {
-        cell.alignment = style.alignment;
+        cell.alignment = { ...style.alignment, wrapText: true };
       } else {
         cell.alignment = { vertical: "middle" };
       }
@@ -176,6 +131,39 @@ export class ExcelService {
   }
   private mergeRowCells(ws, row, from, to) {
     ws.mergeCells(`${row.getCell(from)._address}:${row.getCell(to)._address}`);
+  }
+  public async saveAsExcelFile(wb: any, fileName: string): Promise<void> {
+    const buffer = await wb.xlsx.writeBuffer();
+    const data: Blob = new Blob([buffer], {
+      type: this.EXCEL_TYPE,
+    });
+    fs.saveAs(data, fileName + new Date().getTime() + this.EXCEL_EXTENSION);
+  }
+  public generateWorkbook(): Workbook {
+    return new Workbook();
+  }
+  public addWorksheet(wb: Workbook, sheetName: string): any {
+    return wb.addWorksheet(sheetName);
+  }
+  public addRowTitle(
+    workSheet: any,
+    title: string,
+    from: number,
+    to: number,
+    style: any
+  ): any {
+    let rowSheetTitle = this.addRow(workSheet, [title], style);
+    this.mergeRowCells(workSheet, rowSheetTitle, from, to);
+  }
+  public styleWidthColumns(workSheet: any, widths: { width: number }[]) {
+    if (widths && widths.length > 0) {
+      workSheet.columns = widths;
+    }
+  }
+  private addEmptyRow(workSheet: any, numberRow: number = 1) {
+    for (let index = 0; index < numberRow; index++) {
+      workSheet.addRow([]);
+    }
   }
 }
 
@@ -196,7 +184,7 @@ export class ExcelService {
 
 // worksheet.getColumn(3).width = 30;
 // worksheet.getColumn(4).width = 30;
-
+// rowHeader.splice(7, 0, "", "");
 // // Add row title and formatting
 // const title = "Hole section summary";
 // const titleRow = worksheet.addRow([title]);
